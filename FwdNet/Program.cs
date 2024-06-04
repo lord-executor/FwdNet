b4877fa7cc3196a187b663bc4eae36bd5cfb00d0
Lukas Angerer <executor.dev@gmail.com>
2024-06-04 23:31:12 +0200

    Testing configurability

--- FwdNet/Program.cs
+++ FwdNet/Program.cs
@@ -1,5 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Notes:
+// - https://learn.microsoft.com/en-us/dotnet/core/additional-tools/self-signed-certificates-guide (With PowerShell)
+// - https://learn.microsoft.com/en-us/aspnet/core/fundamentals/servers/kestrel/endpoints?view=aspnetcore-8.0#configure-https-in-code
+
+builder.WebHost.UseUrls("https://127.0.42.1");
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
